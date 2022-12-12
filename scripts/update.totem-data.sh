e0956725542414b6b58e5fc53e71dea15125e944
Gonzalo Vera <gonzalo.vera@cragenomica.es>
2022-12-12 12:54:19 +0100

    modified:   update.totem-data.sh

--- scripts/update.totem-data.sh
+++ scripts/update.totem-data.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "reaching totem-data"
-wget --no-check-certificate https://bioinformatics.cragenomica.es/totem-data/test.txt
+#echo "reaching totem-data"
+#wget --no-check-certificate https://bioinformatics.cragenomica.es/totem-data/test.txt
+echo "Copying whole experiments folder"
+mkdir /srv/actions-runner/_work/totem/totem/experiments && \
+cd /srv/actions-runner/_work/totem/totem/experiments && \
+wget --no-verbose --no-parent --recursive \
+https://bioinformatics.cragenomica.es/totem-data/experiments/
 
