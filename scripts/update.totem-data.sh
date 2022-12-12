0f69c9a2315be7ed6b997bff5ed40e519772ba88
Gonzalo Vera <gonzalo.vera@cragenomica.es>
2022-12-12 16:50:40 +0100

    modified:   update.totem-data.sh

--- scripts/update.totem-data.sh
+++ scripts/update.totem-data.sh
@@ -3,8 +3,17 @@
 #echo "reaching totem-data"
 #wget --no-check-certificate https://bioinformatics.cragenomica.es/totem-data/test.txt
 echo "Copying whole experiments folder"
+date && \
+rm -rf /srv/actions-runner/_work/totem/totem/experiments && \
 mkdir /srv/actions-runner/_work/totem/totem/experiments && \
 cd /srv/actions-runner/_work/totem/totem/experiments && \
 wget --no-verbose --no-parent --recursive \
-https://bioinformatics.cragenomica.es/totem-data/experiments/
+--continue \
+--no-host-directories \
+--cut-dirs=1 \
+-P /srv/actions-runner/_work/totem/totem/ \
+-R '\?C=' \
+https://bioinformatics.cragenomica.es/totem-data/experiments/ && \
+find /srv/actions-runner/_work/totem/totem/experiments -regextype posix-egrep -type f -regex '^(.*?html\?C=[DNSM];O=[AD])$' -print -exec rm {} \; && \
+find /srv/actions-runner/_work/totem/totem/experiments -regextype posix-egrep -type f -regex 'index.html$' -print -exec rm {} \; && date 
 
